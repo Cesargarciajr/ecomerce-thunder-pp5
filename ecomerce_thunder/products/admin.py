25be42adcaa14d6f8dc93a9e5408d4f4f1a10119
Cesar Garcia <cesargarcia.elder@gmail.com>
2024-02-11 13:30:43 +0000

    Customized admin

--- ecomerce_thunder/products/admin.py
+++ ecomerce_thunder/products/admin.py
@@ -2,5 +2,30 @@ from django.contrib import admin
 from .models import Product, Category
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+
+class ProductAdmin(admin.ModelAdmin):
+    """
+    Product class to django admin
+    """
+    list_display = (
+        'name',
+        'category',
+        'price',
+        'stock_number',
+        'image',
+    )
+
+    ordering = ('name',) # determining ordering to be displayed in the list
+
+class CategoryAdmin(admin.ModelAdmin):
+    """
+    Category class to django admin
+    """
+    list_display = (
+            'display_name',
+            'name',
+    )
+
+# Added the classes to the admin django panel
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
--- ecomerce_thunder/products/models.py
+++ ecomerce_thunder/products/models.py
@@ -3,6 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Category(models.Model):
+    
+    class Meta:
+        verbose_name_plural = 'Categories' # Fixing how 'Categories' will be displayed on Django Admin
+
     name = models.CharField(max_length=254)
     display_name = models.CharField(max_length=254, null=True, blank=True)
 
