06348ad7d788ebecda8c5da8e2302ee54e467607
Cesar Garcia <cesargarcia.elder@gmail.com>
2024-02-12 23:57:04 +0000

    Added sorting and filtering options

--- ecomerce_thunder/products/views.py
+++ ecomerce_thunder/products/views.py
@@ -11,8 +11,30 @@ def all_products(request):
     products = Product.objects.all()
     query = None
     categories = None
+    sort = None
+    direction = None
 
     if request.GET:
+
+        # checking if there is a filter by price or category requested
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'            
+            # checking if filter is ascendent or descendent
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+            
+
         # checking if there is a filter by category requested
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
@@ -30,11 +52,14 @@ def all_products(request):
             # checking if search criteria is in the name or description of a product. the 'icontains' make it case-insentive
             queries = Q(name__icontains=query) | Q(description__icontains=query) 
             products = products.filter(queries)
+    
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
