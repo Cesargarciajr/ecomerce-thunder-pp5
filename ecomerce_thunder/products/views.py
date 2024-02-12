855bc42d2544325d06b45067dbc32ee7cb2cb43d
Cesar Garcia <cesargarcia.elder@gmail.com>
2024-02-12 01:42:19 +0000

    Added search engine for products

--- ecomerce_thunder/products/views.py
+++ ecomerce_thunder/products/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q # optimises search criteria for name or description
 from .models import Product
 
 # Create your views here.
@@ -7,6 +9,18 @@ def all_products(request):
     """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
+    query = None
+
+    if request.GET: # checkinf if there is a search being made at the search bar/form 
+        if 'search' in request.GET:
+            query = request.GET['search']
+            if not query:
+                messages.error(request, "Please enter a search!")
+                return redirect(reverse('products'))
+
+            # checking if search criteria is in the name or description of a product. the 'icontains' make it case-insentive
+            queries = Q(name__icontains=query) | Q(description__icontains=query) 
+            products = products.filter(queries)
 
     context = {
         'products': products,
