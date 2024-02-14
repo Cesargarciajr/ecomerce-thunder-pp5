163648f77b1c61cfe064976d26af8fd72ac22933
Cesar Garcia <cesargarcia.elder@gmail.com>
2024-02-14 02:14:49 +0000

    Added shopping cart context and delivery context processor

--- ecomerce_thunder/ecomerce_thunder/settings.py
+++ ecomerce_thunder/ecomerce_thunder/settings.py
@@ -75,6 +75,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth', # required by allauth
                 'django.contrib.messages.context_processors.messages',
+                'shopping_cart.contexts.shopping_cart_contents', # content processor for shopping cart content
             ],
         },
     },
@@ -165,3 +166,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+FREE_DELIVERY = 35
+STANDARD_DELIVERY = 15
---
+++ ecomerce_thunder/shopping_cart/contexts.py
@@ -0,0 +1,32 @@
+from decimal import Decimal
+from djnago.con import settings
+
+
+# Context processor to make this dictionary available through the entire application which added in the settings.py context processor array
+
+
+def shopping_cart_contents(request):
+
+    shopping_cart_items = []
+    total = 0
+    product_count = 0
+
+    if total < settings.FREE_DELIVERY:
+        delivery = total * Decimal(settings.STANDARD_DELIVERY)
+        get_free_delivery = settings.FREE_DELIVERY - total
+    else:
+        delivery = 0
+        get_free_delivery = 0
+
+    grand_total = delivery + total
+
+    context = {
+        'shopping_cart_items': shopping_cart_items,
+        'total': total,
+        'product_count': product_count,
+        'get_free_delivery': get_free_delivery,
+        'free_delivery': settings.FREE_DELIVERY
+        'grand_total': grand_total,
+    }
+
+    return context
