9d48c35f5bec5d0f0509c84160512915330fa4d1
Cesar Garcia <cesargarcia.elder@gmail.com>
2024-02-22 21:53:09 +0000

    Added basic stripe functionality

--- ecomerce_thunder/checkout/views.py
+++ ecomerce_thunder/checkout/views.py
@@ -1,21 +1,41 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from shopping_cart.contexts import shopping_cart_contents
 
+import stripe
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     shopping_cart = request.session.get('shopping_cart', {})
     if not shopping_cart:
         messages.error(request, "There's nothing in your shopping cart at the moment")
         return redirect(reverse('products'))
 
+    current_shopping_cart = shopping_cart_contents(request)
+    total = current_shopping_cart['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set in your environment?')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51OmPWUI8NvNINQ7FUSsIoYm6FsgdeACAJ1UJtpX5FFVYcsqlUnr8CYWL6oKUxaRNj8EIVGcwqF8pUJlFFa9T0Fd500Vg9iABpQ',
-        'client_secret': 'sk_test_51OmPWUI8NvNINQ7Fy55CSLbchccuFkEP6aovwH2k8xTFXPj6NYDuLhDa1jXv608hJ8QV7FtptrHud7jnNO7BJeT1008lnVR3tW',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
