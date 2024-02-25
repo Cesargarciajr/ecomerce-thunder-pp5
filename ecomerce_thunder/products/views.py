7b6c09558027d5fcb6246998a61c44f1a55295a0
Cesar Garcia <cesargarcia.elder@gmail.com>
2024-02-25 16:57:19 +0000

    Added add product view, url, template and form

--- ecomerce_thunder/products/views.py
+++ ecomerce_thunder/products/views.py
@@ -4,6 +4,7 @@ from django.db.models import Q # optimises search criteria for name or descripti
 from django.db.models.functions import Lower
 
 from .models import Product, Category
+from .forms import ProductForm
 
 # Create your views here.
 
@@ -77,3 +78,14 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+
+def add_product(request):
+    """ Add a product to the store """
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
