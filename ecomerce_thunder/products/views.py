62ffbddbc7a183535184a0ca9205bcaaaeac3e19
Cesar Garcia <cesargarcia.elder@gmail.com>
2024-02-26 09:53:40 +0000

    Added login required decorator to add, edit and delete products

--- ecomerce_thunder/products/views.py
+++ ecomerce_thunder/products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q # optimises search criteria for name or description
 from django.db.models.functions import Lower
+from django.contrib.auth.decorators import login_required
 
 from .models import Product, Category
 from .forms import ProductForm
@@ -79,7 +80,7 @@ def product_detail(request, product_id):
 
     return render(request, 'products/product_detail.html', context)
 
-
+@login_required
 def add_product(request):
     """ Add a product to the store """
     if request.method == 'POST':
@@ -100,7 +101,7 @@ def add_product(request):
 
     return render(request, template, context)
 
-
+@login_required
 def edit_product(request, product_id):
     """ Edit a product in the store """
     product = get_object_or_404(Product, pk=product_id)
@@ -124,7 +125,7 @@ def edit_product(request, product_id):
 
     return render(request, template, context)
 
-
+@login_required
 def delete_product(request, product_id):
     """ Delete a product from the store """
     product = get_object_or_404(Product, pk=product_id)
