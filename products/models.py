22b64a2551d04f441f72960e16129474103c6255
Cesar Garcia <cesargarcia.elder@gmail.com>
2024-05-16 15:43:26 +0000

    fixed add_product form to invalid negative price

--- products/models.py
+++ products/models.py
@@ -21,11 +21,11 @@ class Category(models.Model):
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     stock_number = models.CharField(max_length=254, null=True, blank=True)
-    name = models.CharField(max_length=254, blank=False)
+    name = models.CharField(max_length=254, blank=False, null=False)
     description = models.TextField(blank=False)
     price = models.DecimalField(max_digits=6, decimal_places=2, blank=False)
-    image_url = models.URLField(max_length=1024, null=True, blank=False)
-    image = models.ImageField(null=True, blank=False)
+    image_url = models.URLField(max_length=1024, null=False, blank=False)
+    image = models.ImageField(null=False, blank=False)
 
     def __str__(self):
         return self.name
