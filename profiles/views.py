0c4247e85cba7382464d9a0a9b348ec64caf9eec
Cesar Garcia <cesargarcia.elder@gmail.com>
2024-05-18 09:50:46 +0000

    Added newsletters mailchimp also fixed footer layout

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from shopping_cart.views import add_to_shopping_cart
