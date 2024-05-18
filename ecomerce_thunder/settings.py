8e136aafe1ae7bf552457aafe252e11e1f7d2152
Cesar Garcia <cesargarcia.elder@gmail.com>
2024-05-18 17:04:03 +0000

    fixed li element in the nav bar as html report issued

--- ecomerce_thunder/settings.py
+++ ecomerce_thunder/settings.py
@@ -95,7 +95,7 @@ TEMPLATES = [
 ]
 
 # Django-Allauth backends needed settings
-CSRF_TRUSTED_ORIGINS = ['https://8000-cesargarcia-ecomercethu-ntsr9to7ya9.ws-eu111.gitpod.io',
+CSRF_TRUSTED_ORIGINS = ['https://8000-cesargarcia-ecomercethu-ntsr9to7ya9.ws-eu114.gitpod.io',
                         'https://8000-cesargarcia-ecomercethu-ntsr9to7ya9.ws-eu112.gitpod.io'] # Required by Django 4 update https://docs.djangoproject.com/en/dev/releases/4.0/#format-change
 
 # Toasts Messages storage
